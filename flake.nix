83c68f145beec56b8172f8e34b73415bb1294b8a
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-04-03 19:40:05 -0700

    chore(nix): add basic flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Flake for github:philippemerle/KubeDiagrams";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShells.aarch64-darwin.default = let
+      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+    in
+      pkgs.mkShell {
+        packages = with pkgs; [
+          git
+          lazygit
+
+          nodePackages.prettier
+        ];
+      };
+  };
+}
