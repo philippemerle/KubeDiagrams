85f2bd493781ba56ad4520be02fa7383648deb61
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-04-19 18:37:26 -0500

    chore: update flake to match

--- flake.nix
+++ flake.nix
@@ -3,21 +3,53 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
-    devShells.aarch64-darwin.default = let
-      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-    in
-      pkgs.mkShell {
-        packages = with pkgs; [
-          git
-          lazygit
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pythonEnv = pkgs.python312.withPackages (ps: [ ps.pyyaml ps.diagrams ]);
 
+        kube-diagrams = pkgs.stdenv.mkDerivation rec {
+          pname = "kube-diagrams";
+          version = "0.2.0";
+          src = self;
+          postPatch = ''
+            substituteInPlace bin/kube-diagrams \
+              --replace '/usr/bin/env python3' '${pythonEnv}/bin/python'
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            cp -r bin/* $out/bin/
+            chmod +x $out/bin/*-diagrams
+          '';
+        };
+
+        runtimeEnv = with pkgs; [
+          busybox
+          cacert
           graphviz
-          nodePackages.prettier
-          python39
+          kubernetes-helm
+          kube-diagrams
+          pythonEnv
         ];
-      };
-  };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            git
+            lazygit
+            nodePackages.prettier
+          ] ++ runtimeEnv;
+        };
+
+        packages.default = pkgs.dockerTools.buildLayeredImage {
+          name = "ghcr.io/philippemerle/kubediagrams";
+          tag = "latest";
+          contents = runtimeEnv;
+          created = "now";
+        };
+      }
+    );
 }
